468c4f25457471af87d607f10ae0f65578370e78
allworldg <944937881@qq.com>
2020-04-12 23:38:29 +0800

    04 dont finish

--- Code04/app/src/main/java/com/example/code04/MainActivity.java
+++ Code04/app/src/main/java/com/example/code04/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.code04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText etMessage=findViewById(R.id.message);
+
+        Button btnSend=(Button)findViewById(R.id.send_Message);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = etMessage.getText().toString();
+                Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
+
+                Intent intent=new Intent(MainActivity.this,MessageActivity.class;
+                intent.putExtra("sendMessage",message);
+                startActivity(intent);
+            }
+        });
     }
 }
